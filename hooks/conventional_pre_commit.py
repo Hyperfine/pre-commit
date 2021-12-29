def6eb544036d2d33359a3544cf27bcab4829c85
Joel Castillo <jcastillo@hyperfine.io>
2021-12-28 21:21:36 -0500

    feat(conventional-pre-commit): Setup CI/CD with GH Actions; 100% coverage for conventional pre-commit hook

--- hooks/conventional_pre_commit.py
+++ hooks/conventional_pre_commit.py
@@ -5,24 +5,14 @@ import os
 from pathlib import Path
 import subprocess
 import argparse
-
 import toml
 
-DEFAULT_CC_TYPES = ["build" "chore" "ci" "docs" "feat" "fix" "perf" "refactor" "revert" "style" "test"]
+DEFAULT_CC_TYPES = ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"]
 DEFAULT_CC_SCOPES = []
 VALID_COMMIT_START_CHARS = [":", "!:"]
 
 
-def get_git_root():
-    """ Returns the root directory of the git repository, assuming this script is run from within the repository. """
-    result = subprocess.run(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE, check=True)
-    if result.stdout is None:
-        # TODO: concrete exception
-        raise Exception('Did not get any output from git: stderr is "{}"'.format(result.stderr))
-    return result.stdout.decode('utf-8').rstrip('\n')
-
-
-def cc_check(project_dir: str):
+def cc_check(project_dir: str, pyproject_file: str) -> None:
     """Retrieve the git commit message and check it for CC style.
 
     CC stands for conventional commits [https://conventionalcommits.org]
@@ -30,19 +20,34 @@ def cc_check(project_dir: str):
 
     Args:
         project_dir (str): The root directory of the project.
+        pyproject_file (str): The path to the `pyproject.toml`
     """
     # Retrieve conventional commit configuration from pyproject.toml
-    pyproject_file = project_dir / "pyproject.toml"
     toml_dict = toml.load(open(pyproject_file))
 
     # Need to retrieve types list from pyproject.toml.
-    cc_types = toml_dict["tool"]["conventional_commit_check"]["types"] if toml_dict["tool"]["conventional_commit_check"]["types"] is not None else DEFAULT_CC_TYPES
+    try:
+        cc_types = toml_dict["tool"]["conventional_commit_check"]["types"] if toml_dict["tool"]["conventional_commit_check"]["types"] is not None else DEFAULT_CC_TYPES
+    except KeyError:
+        cc_types = DEFAULT_CC_TYPES
     # Need to retrieve scope list from pyproject.toml.
-    cc_scopes = toml_dict["tool"]["conventional_commit_check"]["scopes"] if toml_dict["tool"]["conventional_commit_check"]["scopes"] is not None else DEFAULT_CC_TYPES
+    try:
+        cc_scopes = toml_dict["tool"]["conventional_commit_check"]["scopes"] if toml_dict["tool"]["conventional_commit_check"]["scopes"] is not None else DEFAULT_CC_TYPES
+    except KeyError:
+        cc_scopes = DEFAULT_CC_SCOPES
 
     # Retrieve commit message.
     msg_file = project_dir / ".git" / "COMMIT_EDITMSG"
-    commit_msg = open(msg_file).read()
+    commit_msg_lines = open(msg_file).readlines()
+
+    # Parse the commit message to remove any ignored lines.
+    commit_msg = []
+    for line in commit_msg_lines:
+        if line[0] == "#":
+            continue
+        commit_msg.append(line)
+
+    commit_msg = '\n'.join(commit_msg).strip()
 
     error_msg = f"""Commit message:
 {commit_msg}
@@ -51,10 +56,10 @@ does not follow Conventional Commits formatting.
 https://www.conventionalcommits.org/
 
 Conventional Commits start with one of the below types:
-{cc_types}
+{','.join(cc_types)}
 
 followed by the scope in parentheses which is one of:
-{cc_scopes}
+{','.join(cc_scopes)}
 
 followed by a colon and a space, followed by the commit message.
 
@@ -72,6 +77,9 @@ Example:
 
     BREAKING CHANGE: removes support for tensorflow 2
 """
+    # Validate that the commit message is not just an empty string
+    if not commit_msg:
+        raise ValueError(error_msg)
 
     # Validate the type in the commit message.
     cc_type_start_index = 0
@@ -81,17 +89,20 @@ Example:
         raise ValueError(error_msg)
 
     # Validate the scope in the commit message.
-    cc_scope_start_index = cc_type_end_index
+    cc_scope_start_index = cc_type_end_index + 1
     cc_scope_end_index = commit_msg.index(")")
     cc_scope = commit_msg[cc_scope_start_index:cc_scope_end_index]
     if cc_scopes and cc_scope not in cc_scopes:
+        print(cc_scope)
+        print(cc_scopes)
         raise ValueError(error_msg)
     commit_msg = commit_msg[cc_scope_end_index + 1:]
 
     # Validate that the commit message begins with either a ":" or "!:" (for breaking changes).
     breaking_change = commit_msg[0] == "!"
-    colon_index = commit_msg.index(":")
-    if commit_msg[:colon_index + 1] not in VALID_COMMIT_START_CHARS:
+    try:
+        colon_index = commit_msg.index(":")
+    except ValueError:
         raise ValueError(error_msg)
     commit_msg = commit_msg[colon_index + 1:]
 
@@ -100,14 +111,17 @@ Example:
         raise ValueError(error_msg)
 
     # Validate that the breaking change description exists in the commit, if this is a breaking change.
-    if breaking_change and "BREAKING CHANGE:" not in commit_msg:
-        raise ValueError(error_msg)
-    breaking_change_start_index = commit_msg.index("BREAKING CHANGE:")
-    breaking_change_end_index = breaking_change_start_index + len("BREAKING CHANGE:")
-    breaking_change_msg = commit_msg[breaking_change_end_index + 1:]
+    if breaking_change:
+        if "BREAKING CHANGE:" not in commit_msg:
+            raise ValueError(error_msg)
+        breaking_change_start_index = commit_msg.index("BREAKING CHANGE:")
+        breaking_change_end_index = breaking_change_start_index + len("BREAKING CHANGE:")
+        breaking_change_msg = commit_msg[breaking_change_end_index + 1:]
 
-    # Validate that the description of the breaking change is non-zero length.
-    if breaking_change and not breaking_change_msg:
+        # Validate that the description of the breaking change is non-zero length.
+        if breaking_change and not breaking_change_msg:
+            raise ValueError(error_msg)
+    if not breaking_change and "BREAKING CHANGE:" in commit_msg:
         raise ValueError(error_msg)
 
 
@@ -119,12 +133,22 @@ def parse_args():
         ),
     )
     parser.add_argument(
-        'project_dir',
+        '--project_dir',
+        dest='project_dir',
         metavar='PROJECT_DIR',
         type=str,
         nargs='?',
         default=os.getcwd(),
         help='The project directory. Defaults to the current working directory.',
+    )
+    parser.add_argument(
+        '--toml',
+        dest='toml',
+        metavar='PYPROJECT_TOML',
+        type=str,
+        nargs='?',
+        default=os.path.join(os.getcwd(), "pyproject.toml"),
+        help='The path to the `pyproject.toml` file. Defaults to `pyproject.toml` in the current working directory.',
     )
     args = parser.parse_args()
     return args
@@ -132,11 +156,10 @@ def parse_args():
 
 def main():
     args = parse_args()
+    print(args)
     project_dir = Path(args.project_dir)
-    try:
-        cc_check(project_dir=project_dir)
-    except ValueError as exc:
-        raise Exception().with_traceback(exc.__traceback__)
+    pyproject_file = Path(args.toml)
+    cc_check(project_dir=project_dir, pyproject_file=pyproject_file)
 
 
 if __name__ == '__main__':
