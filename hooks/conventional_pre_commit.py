de2a5ceae8ddb41c4035ec58a53b061d9a83aeca
Joel Castillo <jcastillo@hyperfine.io>
2022-01-03 18:46:44 -0500

    feat(conventional-pre-commit): Add conventional pre-commit python script with testing

---
+++ hooks/conventional_pre_commit.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+"""Python script to check for conventional commit style."""
+
+import os
+from pathlib import Path
+import subprocess
+import argparse
+import toml
+
+DEFAULT_CC_TYPES = ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"]
+DEFAULT_CC_SCOPES = []
+VALID_COMMIT_START_CHARS = [":", "!:"]
+
+
+def cc_check(project_dir: str, pyproject_file: str) -> None:
+    """Retrieve the git commit message and check it for CC style.
+
+    CC stands for conventional commits [https://conventionalcommits.org]
+    Not using regex.
+
+    Args:
+        project_dir (str): The root directory of the project.
+        pyproject_file (str): The path to the `pyproject.toml`
+    """
+    # Retrieve conventional commit configuration from pyproject.toml
+    toml_dict = toml.load(open(pyproject_file))
+
+    # Need to retrieve types list from pyproject.toml.
+    try:
+        cc_types = toml_dict["tool"]["conventional_commit_check"]["types"] if toml_dict["tool"]["conventional_commit_check"]["types"] is not None else DEFAULT_CC_TYPES
+    except KeyError:
+        cc_types = DEFAULT_CC_TYPES
+    # Need to retrieve scope list from pyproject.toml.
+    try:
+        cc_scopes = toml_dict["tool"]["conventional_commit_check"]["scopes"] if toml_dict["tool"]["conventional_commit_check"]["scopes"] is not None else DEFAULT_CC_TYPES
+    except KeyError:
+        cc_scopes = DEFAULT_CC_SCOPES
+
+    # Retrieve commit message.
+    msg_file = project_dir / ".git" / "COMMIT_EDITMSG"
+    commit_msg_lines = open(msg_file).readlines()
+
+    # Parse the commit message to remove any ignored lines.
+    commit_msg = []
+    for line in commit_msg_lines:
+        if line[0] == "#":
+            continue
+        commit_msg.append(line)
+
+    commit_msg = '\n'.join(commit_msg).strip()
+
+    error_msg = f"""Commit message:
+{commit_msg}
+
+does not follow Conventional Commits formatting.
+https://www.conventionalcommits.org/
+
+Conventional Commits start with one of the below types:
+{','.join(cc_types)}
+
+followed by the scope in parentheses which is one of:
+{','.join(cc_scopes)}
+
+followed by a colon and a space, followed by the commit message.
+
+Example:
+    fix(recon): remove infinite loop
+
+If your commit contains a breaking change, then a "!" should come
+before the colon and your commit message must have "BREAKING CHANGE:"
+with a description of the change as it's last line.
+
+Example:
+    fix(recon)!: update to tensorflow 3
+
+    This makes changes to support only tensorflow 3.
+
+    BREAKING CHANGE: removes support for tensorflow 2
+"""
+    # Validate that the commit message is not just an empty string
+    if not commit_msg:
+        raise ValueError(error_msg)
+
+    # Validate the type in the commit message.
+    cc_type_start_index = 0
+    cc_type_end_index = commit_msg.index("(")
+    cc_type = commit_msg[cc_type_start_index:cc_type_end_index]
+    if cc_type not in cc_types:
+        raise ValueError(error_msg)
+
+    # Validate the scope in the commit message.
+    cc_scope_start_index = cc_type_end_index + 1
+    cc_scope_end_index = commit_msg.index(")")
+    cc_scope = commit_msg[cc_scope_start_index:cc_scope_end_index]
+    if cc_scopes and cc_scope not in cc_scopes:
+        print(cc_scope)
+        print(cc_scopes)
+        raise ValueError(error_msg)
+    commit_msg = commit_msg[cc_scope_end_index + 1:]
+
+    # Validate that the commit message begins with either a ":" or "!:" (for breaking changes).
+    breaking_change = commit_msg[0] == "!"
+    try:
+        colon_index = commit_msg.index(":")
+    except ValueError:
+        raise ValueError(error_msg)
+    commit_msg = commit_msg[colon_index + 1:]
+
+    # Validate that the commit message actually exists
+    if not commit_msg:
+        raise ValueError(error_msg)
+
+    # Validate that the breaking change description exists in the commit, if this is a breaking change.
+    if breaking_change:
+        if "BREAKING CHANGE:" not in commit_msg:
+            raise ValueError(error_msg)
+        breaking_change_start_index = commit_msg.index("BREAKING CHANGE:")
+        breaking_change_end_index = breaking_change_start_index + len("BREAKING CHANGE:")
+        breaking_change_msg = commit_msg[breaking_change_end_index + 1:]
+
+        # Validate that the description of the breaking change is non-zero length.
+        if breaking_change and not breaking_change_msg:
+            raise ValueError(error_msg)
+    if not breaking_change and "BREAKING CHANGE:" in commit_msg:
+        raise ValueError(error_msg)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=(
+            'A CLI for checking commit messages against the conventional commit standard '
+            'Optionally takes in a project directory to check. Defaults to the current working directory.'
+        ),
+    )
+    parser.add_argument(
+        '--project_dir',
+        dest='project_dir',
+        metavar='PROJECT_DIR',
+        type=str,
+        nargs='?',
+        default=os.getcwd(),
+        help='The project directory. Defaults to the current working directory.',
+    )
+    parser.add_argument(
+        '--toml',
+        dest='toml',
+        metavar='PYPROJECT_TOML',
+        type=str,
+        nargs='?',
+        default=os.path.join(os.getcwd(), "pyproject.toml"),
+        help='The path to the `pyproject.toml` file. Defaults to `pyproject.toml` in the current working directory.',
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    print(args)
+    project_dir = Path(args.project_dir)
+    pyproject_file = Path(args.toml)
+    cc_check(project_dir=project_dir, pyproject_file=pyproject_file)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/conventional_pre_commit_test.py
@@ -0,0 +1,194 @@
+import os
+import sys
+import glob
+import unittest
+import tempfile
+from pathlib import Path
+
+if sys.version_info[0] < 3:
+    import subprocess32 as subprocess
+else:
+    import subprocess
+
+COMMIT_MESSAGE_FOOTER_TEMPLATE = """{commit_msg}
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# Date:      Tue Dec 28 19:35:36 2021 -0500
+#
+# On branch master
+# Your branch is ahead of 'origin/master' by 106 commits.
+#
+# Changes to be committed:
+#	modified:   .pre-commit-hooks.yaml
+#
+# Untracked files:
+#
+"""
+
+
+class TestConventionalPreCommit(unittest.TestCase):
+    def setUp(self):
+        self.root = get_git_root()
+        self.hook_script = os.path.join(self.root, 'hooks', 'conventional_pre_commit.py')
+        self.fixture_dir = os.path.join(self.root, 'test', 'fixtures')
+        self.invalid_project_dir = tempfile.mkdtemp()
+        self.invalid_git_dir = os.path.join(self.invalid_project_dir, "git")
+        self.project_dir = tempfile.mkdtemp()
+        self.git_dir = os.path.join(self.project_dir, ".git")
+        Path(self.git_dir).mkdir(parents=True, exist_ok=True)
+
+    def _check_success(self, project_dir, pyproject_toml):
+        subprocess.run([self.hook_script, "--project_dir", project_dir, "--toml", pyproject_toml], check=True)
+
+    def _check_failure(self, project_dir, pyproject_toml):
+        result = subprocess.run([self.hook_script, "--project_dir", project_dir, "--toml", pyproject_toml], stderr=subprocess.PIPE)
+        self.assertEqual(result.returncode, 1)
+
+    def test_invalid_git_dir(self):
+        self._check_failure(self.invalid_project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+
+    def test_valid_commit_message_default(self):
+        commit_msg = "build(precommit): This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_success(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+
+    def test_invalid_commit_message_default(self):
+        commit_msg = "This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+
+    def test_invalid_commit_message_default_empty_commit(self):
+        commit_msg = ""
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+
+    def test_invalid_commit_message_default_empty_commit_description(self):
+        commit_msg = "build(precommit):"
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+    
+    def test_invalid_commit_message_default_invalid_commit_separator(self):
+        commit_msg = "build(precommit)!"
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+   
+    def test_invalid_commit_message_default_no_scope(self):
+        commit_msg = "build: This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+
+    def test_invalid_commit_message_default_no_type(self):
+        commit_msg = "(build): This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+
+    def test_valid_commit_message_required_scope(self):
+        commit_msg = "build(tools): This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_success(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.scope_only'))
+
+    def test_invalid_commit_message_required_scope(self):
+        commit_msg = "build(precommit): This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.scope_only'))
+
+    def test_valid_commit_message_required_type(self):
+        commit_msg = "feat(tools): This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_success(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.type_only'))
+
+    def test_invalid_commit_message_required_type(self):
+        commit_msg = "chore(precommit): This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.type_only'))
+
+    def test_valid_commit_message_required_type_and_scope(self):
+        commit_msg = "build(config): This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_success(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.complete'))
+
+    def test_invalid_commit_message_required_type_and_scope(self):
+        commit_msg = "chore(chore): This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.complete'))
+
+    def test_invalid_commit_message_required_type_and_scope_invalid_type(self):
+        commit_msg = "chore(config): This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.complete'))
+
+    def test_invalid_commit_message_required_type_and_scope_invalid_scope(self):
+        commit_msg = "build(chore): This is a sample commit message."
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.complete'))
+
+    def test_valid_commit_message_breaking_change(self):
+        commit_msg = """build(config)!: This is a sample commit message.
+
+BREAKING CHANGE: This is a breaking change description.
+"""
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_success(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+
+    def test_invalid_commit_message_breaking_change_missing_title(self):
+        commit_msg = """build(config): This is a sample commit message.
+
+BREAKING CHANGE: This is a breaking change description.
+"""
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+
+    def test_invalid_commit_message_breaking_change_missing_description_callout(self):
+        commit_msg = """build(config)!: This is a sample commit message.
+
+This is a breaking change description.
+"""
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+
+    def test_invalid_commit_message_breaking_change_missing_description(self):
+        commit_msg = """build(config)!: This is a sample commit message.
+
+BREAKING CHANGE:
+"""
+        write_commit_message(COMMIT_MESSAGE_FOOTER_TEMPLATE.format(commit_msg=commit_msg), self.git_dir)
+
+        self._check_failure(self.project_dir, os.path.join(self.fixture_dir, 'pyproject.toml.default'))
+
+
+def write_commit_message(commit_msg: str, git_dir: str) -> None:
+    with open(f"{git_dir}/COMMIT_EDITMSG", 'w') as _:
+        _.write(commit_msg)
+
+
+def get_git_root() -> str:
+    """ Returns the root directory of the git repository, assuming this script is run from within the repository. """
+    result = subprocess.run(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE, check=True)
+    if result.stdout is None:
+        # TODO: concrete exception
+        raise Exception('Did not get any output from git: stderr is "{}"'.format(result.stderr))
+    return result.stdout.decode('utf-8').rstrip('\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
