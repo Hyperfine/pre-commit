bb0dc19c418b01f790c5c6b8a946645aa37b6b93
Antero Mejr <antero@mailbox.org>
2022-04-15 15:14:15 -0400

    fix: bugfixes for new hooks, add easier way to test them

--- hooks/forbidden_files.py
+++ hooks/forbidden_files.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Script to prevent git editing of a list of protected files."""
 
+import argparse
 import subprocess
 import logging
 from pathlib import Path
@@ -21,27 +22,23 @@ def protect_files(fpaths: List[Path], cwd: Path) -> None:
 Unstage the file or restore it to its original contents.""")
 
 
-def _get_file_list(paths_file: Path, paths_str: str) -> List[Path]:
+def _get_file_list(paths_file: Path, paths_list: list, cwd: Path) -> List[Path]:
     path_set = set()
     if paths_file.is_file():
         with open(paths_file) as f:
             for line in f:
                 path_set.add(paths_file.parent / Path(line.strip()))
-    str_paths = paths_str.split()
-    for str_path in str_paths:
-        fpath = Path(str_path)
-        if fpath.is_file():
-            path_set.add(fpath)
+    [path_set.add(cwd / Path(x)) for x in paths_list]
     return list(path_set)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser(description="Forbid certain file edits.")
-    parser.add_argument("-l", "--list", type="str", default="",
+    parser.add_argument("-l", "--list", type=str, default="",
                         help="File, list of relative paths to protect.")
-    parser.add_argument("-f", "--files", type="str", default="", nargs="+",
+    parser.add_argument("-f", "--files", type=str, default="", nargs="+",
                         help="Whitespace-separated paths to protect.")
     args = parser.parse_args()
-    file_list = _get_file_list(Path(args.list), args.files)
-    _protect_files(file_list, cwd=Path.cwd())
+    file_list = _get_file_list(Path(args.list), args.files, Path.cwd())
+    protect_files(file_list, Path.cwd())
